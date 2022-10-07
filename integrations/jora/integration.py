5956b982f58c9e0a3e99f845d7e352f7bb51d520
RCovery <ryanczpereira@gmail.com>
2022-10-06 21:07:35 -0300

    chore: application module

--- integrations/jora/integration.py
+++ integrations/jora/integration.py
@@ -3,15 +3,15 @@ from requests import get
 class Jora:
     url = 'jora.com'
     region = 'us'
+    search = ''
+    location = ''
 
     predefined_regions = ['us', 'ca', 'br', 'uk', 'pt']
 
-    def __init__(self, search, location):
-        self.search = search
-        self.location = location
+    # def __init__(self):
 
-    """ def get_results(self, region):
-        if (region == '*'): """
+    # def get_results(self, region, search = '', location = ''):
+    #     if (region == '*'):
 
     def request(self):
         get(f'https://{self.region}.{self.url}/j?q={self.search}&l={self.location}')
--- integrations/manager.py
+++ integrations/manager.py
@@ -1,1 +1,16 @@
-print('Hello world!')
+from .jora import Jora
+
+class Manager:
+    def __init__(self, integration = 'jora'):
+        match (integration):
+            case 'jora':
+                pass
+            case _:
+                self.instance = Jora()
+
+    def get(self):
+        self.instance.request()
+
+    @property
+    def region(self, region = 'us'):
+        self.instance.region = region
